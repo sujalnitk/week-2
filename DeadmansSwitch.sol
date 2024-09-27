bdea2224598d5c6b98f58da2026225002f1555ad
GhostBYte <123203175+sujalnitk@users.noreply.github.com>
2024-09-27 17:08:20 +0000

    updated deadman switch

--- DeadmansSwitch.sol
+++ DeadmansSwitch.sol
@@ -2,34 +2,34 @@
 pragma solidity ^0.8.0;
 
 contract DeadmansSwitch {
-    // TODO: Declare state variables
-    // Hint: You'll need variables for the owner, beneficiary, and last check-in block
+    address owner;
+    address beneficiary;
+    uint lastCheckInBlock;
 
-    // TODO: Implement constructor
     constructor(address _beneficiary) {
-        // Hint: Initialize state variables
+        owner = msg.sender;
+        beneficiary = _beneficiary;
+        lastCheckInBlock = block.number;
     }
 
-    // TODO: Implement still_alive function
     function still_alive() public {
-        // Hint: Update the last check-in block
+        if (msg.sender == owner) {
+            lastCheckInBlock = block.number;
+        }
     }
 
-    // TODO: Implement release funds function
     function releaseFunds() public {
-        // Hint: Check if 10 blocks have passed since last check-in
-        // If so, transfer the contract balance to the beneficiary
+        if (block.number > lastCheckInBlock + 10) {
+            payable(beneficiary).transfer(address(this).balance);
+        }
     }
 
-    // TODO: Implement receive function to allow the contract to receive Ether
     receive() external payable {}
 
-    // Helper function for testing (optional)
     function getLastCheckInBlock() public view returns (uint256) {
-        // TODO: Return the last check-in block
+        return lastCheckInBlock;
     }
 
-    // Helper function for testing (optional)
     function getCurrentBlock() public view returns (uint256) {
         return block.number;
     }
